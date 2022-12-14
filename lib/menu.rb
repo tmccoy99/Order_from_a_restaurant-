a732925684e0a997746b60e7c65b4047ba4d4a74
Finn McCoy <finnmccoy99@gmail.com>
2022-12-14 15:07:58 +0000

    Added test for #print when menu has no saved items

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,11 @@
 class Menu
   
-  def initialize(restaurant_name)
+  def initialize(restaurant_name, io = Kernel)
     @restaurant_name = restaurant_name
+    @io = io
   end
   
+  def print
+    @io.puts "Sorry, no items on the menu!"
+  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,9 +1,16 @@
 require "menu"
 
 describe Menu do
-  
-  it "constructs" do
-    Menu.new("Restaurant Name")
+  context "Initially it" do
+    it "constructs" do
+      Menu.new("Restaurant Name")
+    end
+    
+    it "#print prints no item message" do
+      io = double :io
+      menu =  Menu.new("Name", io)
+      expect(io).to receive(:puts).with("Sorry, no items on the menu!")
+      menu.print
+    end
   end
-  
 end
