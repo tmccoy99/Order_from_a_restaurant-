c373267d3f5cc6ecb34da845e714d75833c96782
Finn McCoy <finnmccoy99@gmail.com>
2022-12-14 16:20:39 +0000

    Created placeholder #print method along with accompanying neccessary functions. Passes all current tests.

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,9 +3,24 @@ class Menu
   def initialize(restaurant_name, io = Kernel)
     @restaurant_name = restaurant_name
     @io = io
+    @items = []
+  end
+  
+  def add_items(*items)
+    @items += items
   end
   
   def print
-    @io.puts "Sorry, no items on the menu!"
+    if @items.empty?
+      @io.puts "Sorry, no items on the menu!"
+      return
+    end
+    print_header
+  end
+  
+  private
+  
+  def print_header
+    @io.puts("Welcome to #{@restaurant_name}!\nHere's the menu:")
   end
 end
