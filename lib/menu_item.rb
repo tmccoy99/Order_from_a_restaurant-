9760bdcaf88653c9e098cfa9db7b2ccb6c2a61d5
Finn McCoy <finnmccoy99@gmail.com>
2022-12-14 17:24:24 +0000

    Tested and implemented #price_string method

--- lib/menu_item.rb
+++ lib/menu_item.rb
@@ -3,8 +3,14 @@ class MenuItem
   
   def initialize(name, price, category)
     @name = name
-    @price = price.round(2)
+    @price = price.to_f.round(2)
     @category = category
+  end
+  
+  def price_string
+    str = "£#{@price}"
+    str += "0" if str[-2] == "."
+    str
   end
   
 end
--- spec/menu_item_spec.rb
+++ spec/menu_item_spec.rb
@@ -2,13 +2,19 @@ require "menu_item"
 
 describe MenuItem do
   it "constructs" do
-    MenuItem.new("chips", 10, :sides)
+    MenuItem.new("chips", 10, :Sides)
   end
 
   it "instance variables can be read" do
-    chips = MenuItem.new("Chips", 2.5, :sides)
+    chips = MenuItem.new("Chips", 2.5, :Sides)
     expect(chips.name).to eq "Chips"
-    expect(chips.price).to eq 2.50
-    expect(chips.category).to eq :sides
+    expect(chips.price).to eq 2.5
+    expect(chips.category).to eq :Sides
+  end
+  
+  it "can return its price as a formatted string" do
+    expect(MenuItem.new("", 2.39, :S).price_string).to eq "£2.39"
+    expect(MenuItem.new("", 10.1, :S).price_string).to eq "£10.10"
+    expect(MenuItem.new("", 3, :S).price_string).to eq "£3.00"
   end
 end
