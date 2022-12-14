aa315a8681f5cb105f13cca5a0feddd2067554ca
Finn McCoy <finnmccoy99@gmail.com>
2022-12-14 14:37:28 +0000

    Added test to ensure instance variables can be read

--- spec/menu_item_spec.rb
+++ spec/menu_item_spec.rb
@@ -5,4 +5,10 @@ describe MenuItem do
     MenuItem.new("chips", 10, :sides)
   end
 
+  it "instance variables can be read" do
+    chips = MenuItem.new("Chips", 2.5, :sides)
+    expect(chips.name).to eq "Chips"
+    expect(chips.price).to eq 2.50
+    expect(chips.category).to eq :sides
+  end
 end
