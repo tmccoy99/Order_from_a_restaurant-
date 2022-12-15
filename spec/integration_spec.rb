45c9d0908fa481206a9026c4073a5089425f59ab
Thomas McCoy <finnmccoy99@gmail.com>
2022-12-15 12:04:16 +0000

    Added integration test for Menu#print

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,7 +5,6 @@ require "menu_item"
 describe "Integration testing" do
   
   large_chips = MenuItem.new("Large Chips", 3, :Sides)
-  small_chips = MenuItem.new("Small Chips", 2, :Sides)
   mushy_peas = MenuItem.new("Mushy Peas", 2.5, :Sides)
   cod = MenuItem.new("Cod", 7.7, :Fish)
   haddock = MenuItem.new("Haddock", 8.36, :Fish)
@@ -15,16 +14,28 @@ describe "Integration testing" do
   before(:each) do
     @io = double :io
     @menu = Menu.new("Finn's Fantastic Fish Bar", @io)
-    @menu.add_items(large_chips, small_chips, mushy_peas, cod, haddock,
-    jb_sausage, pea_fritter)
+    @menu.add_items(cod, haddock, jb_sausage, pea_fritter,
+    large_chips, mushy_peas)
   end
   
   context "#print called after items added to menu instance" do
-   xit "prints formatted menu with restaurant_name in heading" do
+   it "prints formatted menu with restaurant_name in heading" do
+    
     expect(@io).to receive(:puts).with("Welcome to Finn's Fantastic Fish Bar!"\
     "\nHere's the menu:").ordered
+    expect(@io).to receive(:puts).with("\n#FISH\n\n").ordered
+    expect(@io).to receive(:puts).with("Cod: £7.70").ordered
+    expect(@io).to receive(:puts).with("Haddock: £8.36").ordered
+    expect(@io).to receive(:puts).with("\n#TRADITIONAL\n\n").ordered
+    expect(@io).to receive(:puts).with("Jumbo Battered Sausage: £2.20").ordered
+    expect(@io).to receive(:puts).with("Pea Fritter: £2.95")
+    expect(@io).to receive(:puts).with("\n#SIDES\n\n").ordered
+    expect(@io).to receive(:puts).with("Large Chips: £3.00").ordered
+    expect(@io).to receive(:puts).with("Mushy Peas: £2.50").ordered
+    
     @menu.print
     end
+
   end
   
 end
