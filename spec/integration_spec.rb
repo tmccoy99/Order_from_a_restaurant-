3cd2800fd416277530bc965ae20b2b2221f009b7
Thomas McCoy <finnmccoy99@gmail.com>
2022-12-17 17:39:07 +0000

    Added test for Order class and refactored other files

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -11,14 +11,14 @@ describe "Integration testing" do
   jb_sausage = MenuItem.new("Jumbo Battered Sausage", 2.2, :Traditional)
   pea_fritter = MenuItem.new("Pea Fritter", 2.95, :Traditional)
   
-  before(:each) do
+  before(:all) do
     @io = double :io
     @menu = Menu.new("Finn's Fantastic Fish Bar", @io)
     @menu.add_items(cod, haddock, jb_sausage, pea_fritter,
     large_chips, mushy_peas)
   end
   
-  context "#print called after items added to menu instance" do
+  context "Menu#print called:" do
    it "prints formatted menu with restaurant_name in heading" do
     
     expect(@io).to receive(:puts).with("Welcome to Finn's Fantastic Fish Bar!"\
@@ -37,17 +37,14 @@ describe "Integration testing" do
     end
 
   end
-  
+
+  context "Order#create called but no items selected" do
+    xit "prints menu, then prints message saying no order placed" do
+    end
+  end
 end
 
-#large_chips = MenuItem.new("Large Chips", 3, :Sides)
-#small_chips = MenuItem.new("Small Chips", 2, :Sides)
-#mushy_peas = MenuItem.new("Mushy Peas", 2.5, :Sides)
-#cod = MenuItem.new("Cod", 7, :Fish)
-#haddock = MenuItem.new("Haddock", 8, :Fish)
-#jb_sausage = MenuItem.new("Jumbo Battered Sausage", 2.2, :Traditional)
-#pea_fritter = MenuItem.new("Pea Fritter", 2.95, :Traditional)
-#fnc_menu = Menu.new("Finn's Fantastic Fish Bar")
-#Menu.print
-# Hello! Welcome to Finn's Fantastic Fish Bar.
-# Here's the menu for you to browse at your pleasure.
+# order = Order(menu)
+# order.select -> menu is printed
+# -> "Which item would you like to add to your order?"
+# -> "To select an item, type in its name. Or if your order is complete, just hit return"
---
+++ spec/order_spec.rb
@@ -0,0 +1,8 @@
+require "order"
+
+describe Order do
+  it "constructs" do
+    order = Order.new(double :menu)
+  end
+end
+  
