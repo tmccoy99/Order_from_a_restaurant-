89c0cbdbe425136f41edd08c779ad31595d001d9
Thomas McCoy <finnmccoy99@gmail.com>
2022-12-19 17:54:00 +0000

    Added first test for #create

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,25 @@
 require "order"
 
 describe Order do
-  it "constructs" do
-    order = Order.new(double :menu)
+
+  before(:all) do
+    # item1 = "Chips: £2.50"
+    # item2 = "Cod: £7.70"
+    # item3 = "Pea Fritter: £2.95"
+    menu_dbl = double :menu
   end
+
+  it "constructs" do
+    order = Order.new(menu_dbl)
+  end
+
+  context "create is called" do
+    
+    it "calls print on saved menu" do
+      expect(menu).to receive(:print)
+    end
+
+  end
+
 end
   
