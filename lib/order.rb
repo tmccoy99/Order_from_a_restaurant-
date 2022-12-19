d297566ad92e9b4896bb33bd06c4d68a23232f32
Thomas McCoy <finnmccoy99@gmail.com>
2022-12-19 19:08:44 +0000

    Added test for whether #create allows selection of items and quantity after printing menu

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,19 @@
 class Order
 
-  def initialize(menu)
+  def initialize(menu, io = Kernel)
     @menu = menu
+    @io = io
   end
 
   def create
     @menu.print
+    @io.puts("Which item would you like to add to your order?")
+    @io.puts("To finish ordering, just hit return")
+    item_name = @io.gets.chomp
+    @io.puts("And how many portions of #{item_name} would you like?")
+    item_quantity = @io.gets.chomp.to_i
+    @io.puts("#{item_quantity} portions of #{item_name} added to order!")
+
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,12 +2,13 @@ require "order"
 
 describe Order do
 
-  before(:all) do
+  before(:each) do
     # item1 = "Chips: £2.50"
     # item2 = "Cod: £7.70"
     # item3 = "Pea Fritter: £2.95"
-    @menu_dbl = double :menu
-    @order = Order.new(@menu_dbl)
+    @menu = double :menu
+    @io = double :io
+    @order = Order.new(@menu, @io)
   end
 
   it "constructs" do
@@ -15,12 +16,17 @@ describe Order do
 
   context "create is called" do
 
-    it "calls print on saved menu" do
-      expect(@menu_dbl).to receive(:print)
+    it "calls #print on @menu, allows items to be selected and then prints receipt" do
+      expect(@menu).to receive(:print).ordered
+      expect(@io).to receive(:puts).with("Which item would you like to add to your order?").ordered
+      expect(@io).to receive(:puts).with("To finish ordering, just hit return").ordered
+      expect(@io).to receive(:gets).and_return("Chips").ordered
+      expect(@io).to receive(:puts).with("And how many portions of Chips would you like?").ordered
+      expect(@io).to receive(:gets).and_return("3").ordered
+      expect(@io).to receive(:puts).with("3 portions of Chips added to order!").ordered
+     
       @order.create
     end
-
-    
 
   end
 
